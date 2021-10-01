b8a345a02a3fc45f7ba902935a302b6beafe0236
D Seidel <67442972+shykitten27@users.noreply.github.com>
2021-10-01 00:00:47 -0500

    Update MovieSQLs.sql

--- MovieSQLs.sql
+++ MovieSQLs.sql
@@ -16,8 +16,83 @@ CREATE TABLE movies (
    FOREIGN KEY (director_id) REFERENCES directors(director_id)
 );
 
-##populate data by running C:\Users\dseid\launchcode\csharp-practice\Chpt17\Studio17-MovieSQLs\movie-buff-data
-##I know how to do this in SQL, not yet in MySQL
+INSERT INTO directors (first_name, last_name, country)
+VALUES ("John", "Lasseter", "USA");
+
+INSERT INTO directors (first_name, last_name, country)
+VALUES ("Brad", "Bird", "USA");
+
+INSERT INTO directors (first_name, last_name, country)
+VALUES ("Jodie", "Foster", "USA"); 
+
+INSERT INTO directors (first_name, last_name, country)
+VALUES ("Ang", "Lee", "Taiwan"); 
+
+insert into directors (first_name, last_name, country)
+values ("Alfonso", "Cuarón", "Mexico");
+
+insert into directors (first_name, last_name, country)
+values ("Guillermo", "del Toro", "Mexico");
+
+insert into directors (first_name, last_name, country)
+values ("Peter", "Jackson", "New Zealand");
+
+insert into directors (first_name, last_name, country)
+values ("Ava", "DuVernay", "USA");
+
+insert into directors (first_name, last_name, country)
+values ("Patty", "Jenkins", "USA");
+
+insert into directors (first_name, last_name, country)
+values ("Haifaa", "al-Mansour", "Saudi Arabia");
+
+INSERT INTO movies (title, year_released, director_id)
+VALUES ("Toy Story", 1995, 1);
+
+INSERT INTO movies (title, year_released, director_id)
+VALUES ("The Incredibles", 2004, 2);
+
+INSERT INTO movies (title, year_released, director_id)
+VALUES ("Money Monster", 2016, 3);
+
+INSERT INTO movies (title, year_released, director_id)
+VALUES ("A Bug's Life", 1998, 1);
+
+INSERT INTO movies (title, year_released, director_id)
+VALUES ("Crouching Tiger, Hidden Dragon", 2000, 4);
+
+INSERT INTO movies (title, year_released, director_id)
+VALUES ("Harry Potter and the Prisoner of Azkaban", 2004, 5);
+
+INSERT INTO movies (title, year_released, director_id)
+VALUES ("Little Man Tate", 1991, 3);
+
+INSERT INTO movies (title, year_released, director_id)
+VALUES ("Roma", 2018, 5);
+
+INSERT INTO movies (title, year_released, director_id)
+VALUES ("The Perfect Candidate", 2019, 10);
+
+INSERT INTO movies (title, year_released, director_id)
+VALUES ("The Shape of Water", 2017, 6);
+
+INSERT INTO movies (title, year_released, director_id)
+VALUES ("The Lord of the Rings: The Return of the King", 2003, 7);
+
+INSERT INTO movies (title, year_released, director_id)
+VALUES ("A Wrinkle in Time", 2018, 8);
+
+INSERT INTO movies (title, year_released, director_id)
+VALUES ("Wonder Woman", 2017, 9);
+
+INSERT INTO movies (title, year_released, director_id)
+VALUES ("Ratatouille", 2007, 2);
+
+INSERT INTO movies (title, year_released, director_id)
+VALUES ("Mary Shelley", 2017, 10);
+
+INSERT INTO movies (title, year_released, director_id)
+VALUES ("Selma", 2014, 8);
 
 select title from movies;
 
@@ -109,3 +184,34 @@ inner join directors
 on movies.director_id=directors.director_id
 where last_name = "Jackson" and first_name="Peter"
 
+ALTER TABLE  movies
+add column money_earned decimal(13,2);
+
+select * from movies;
+
+update movies set money_earned=373554033 where title = "Toy Story"; 
+update movies set money_earned= 631441092 where title = "The Incredibles"; 
+update movies set money_earned= 93300000 where title = "Money Monster"; 
+update movies set money_earned= 200296754 where title = "A Bug's Life"; 
+update movies set money_earned= 213500000 where title = "Crouching Tiger, Hidden Dragon"; 
+update movies set money_earned= 789961033 where title = "Harry Potter and the Prisoner of Azkaban"; 
+update movies set money_earned= 25000000 where title = "Little Man Tate"; 
+update movies set money_earned= 1109544 where title = "Roma"; 	
+update movies set money_earned= 1294943 where title = "The Perfect Candidate";  
+update movies set money_earned= 12640000 where title = "The Shape of Water"; 
+update movies set money_earned= 1146000000.00 where title = "The Lord of the Rings: The Return of the King"; 
+update movies set money_earned= 132700000 where title = "A Wrinkle in Time"; 
+update movies set money_earned= 818058221 where title = "Wonder Woman"; 
+update movies set money_earned= 623700000 where title = "Ratatouille"; 
+update movies set money_earned= 2792316 where title = "Mary Shelley"; 
+update movies set money_earned= 66776576 where title = "Selma"; 
+update movies set money_earned= 31045750 where title = "The Princess Bride"; 
+
+select * from movies;
+
+select title, year_released, money_earned from movies
+order by money_earned desc;
+
+select title, year_released, money_earned from movies
+where money_earned >= 500000000.00 ##more than half billion
+order by money_earned desc;
